6684a41c732ba990f4c4356981be62c81cb1bd7b
Peer Bastian Duensing <Peer@Peers-MBP.fritz.box>
2021-01-17 19:15:35 +0100

    Bearbeitung

--- hello.cpp
+++ hello.cpp
@@ -4,7 +4,13 @@ using namespace std;
 
 int main()
 {
+    //Begrüßung
     cout<<"Hello World";
+    
+    //Message
+    cout<<"\nLamas sind geil.";
+    
+    //Verabschiedung
     cout<<"\nGood bye";
     return 0;
 }
